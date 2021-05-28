504fd5f3cb9343d7c7269bc8d40ab18e71813045
RETR0BL0CK <jackmcgi03@gmail.com>
2021-05-28 04:08:04 +0100

    V3.1.1

--- MP4-Length-Modifier.Designer.cs
+++ MP4-Length-Modifier.Designer.cs
@@ -188,6 +188,7 @@ namespace MP4_Length_Modifier
             this.Controls.Add(this.buttonBrowse);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "MainWindow";
             this.Text = "MP4 Length Modifier";
             ((System.ComponentModel.ISupportInitialize)(this.inputHours)).EndInit();
